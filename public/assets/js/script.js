69d6480e1995d2beb0a1d793d9ea822132812b40
Nazar Dolnyy <nazar.dolnyy@eleks.com>
2020-10-24 22:21:55 +0300

    Added basic modal support

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,22 @@
 const uwcl = (function() {
+  const HIDDEN_CLASS_NAME = 'hidden';
+
+  function changeElementVisibility(element, visibility) {
+    if (element) {
+      if (visibility) {
+        element.classList.remove(HIDDEN_CLASS_NAME);
+      } else {
+        element.classList.add(HIDDEN_CLASS_NAME);
+      }
+    }
+  }
+
+  function handleModalBackdropClick(e, modalId) {
+    if (e.currentTarget === e.target) {
+      closeModal(modalId);
+    }
+  }
+
   function scrollToElement(elementId) {
     const yOffset = 150;  // header height - make dynamic to cover small screen sizes
     const element = document.getElementById(elementId);
@@ -10,7 +28,18 @@ const uwcl = (function() {
     }
   }
 
+  function openModal(modalId) {
+    changeElementVisibility(document.getElementById(modalId), true);
+  }
+
+  function closeModal(modalId) {
+    changeElementVisibility(document.getElementById(modalId), false);
+  }
+
   return {
-    scrollToElement: scrollToElement
+    scrollToElement: scrollToElement,
+    handleModalBackdropClick: handleModalBackdropClick,
+    openModal: openModal,
+    closeModal: closeModal,
   };
 })();
